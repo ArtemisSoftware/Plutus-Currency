01abb5b321be7f157ba04d4fe9cb729a1f89f210
gustavo <gustavo.isaac@gmail.com>
2024-07-04 12:16:36 +0100

    HomeEvent

---
+++ composeApp/src/commonMain/kotlin/presentation/home/HomeEvent.kt
@@ -0,0 +1,8 @@
+package presentation.home
+
+sealed class HomeEvent {
+    data object RefreshRates : HomeEvent()
+//    data object SwitchCurrencies: HomeEvent()
+//    data class SaveSourceCurrencyCode(val code: String): HomeEvent()
+//    data class SaveTargetCurrencyCode(val code: String): HomeEvent()
+}
