ffc88955d81f25d3f58a9bd8fb46834b0c985b05
gustavo <gustavo.isaac@gmail.com>
2024-07-04 12:04:35 +0100

    HomeHeader

---
+++ composeApp/src/commonMain/kotlin/presentation/home/composables/HomeHeader.kt
@@ -0,0 +1,57 @@
+package presentation.home.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import data.dto.CurrencyDto
+import domain.RequestState
+import domain.models.RateStatus
+import getPlatform
+import ui.theme.headerColor
+
+@Composable
+internal fun HomeHeader(
+    status: RateStatus,
+    source: RequestState<CurrencyDto>,
+    target: RequestState<CurrencyDto>,
+    amount: Double,
+    onAmountChange: (Double) -> Unit,
+    onRatesRefresh: () -> Unit,
+    onSwitchClick: () -> Unit,
+//    onCurrencyTypeSelect: (CurrencyType) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(bottomStart = 12.dp, bottomEnd = 12.dp))
+            .background(headerColor)
+            .padding(top = if (getPlatform().name == "Android") 0.dp else 24.dp)
+            .padding(all = 24.dp)
+    ) {
+        Spacer(modifier = Modifier.height(24.dp))
+        RatesStatus(
+            status = status,
+            onRatesRefresh = onRatesRefresh
+        )
+//        Spacer(modifier = Modifier.height(24.dp))
+//        CurrencyInputs(
+//            source = source,
+//            target = target,
+//            onSwitchClick = onSwitchClick,
+//            onCurrencyTypeSelect = onCurrencyTypeSelect
+//        )
+//        Spacer(modifier = Modifier.height(24.dp))
+//        AmountInput(
+//            amount = amount,
+//            onAmountChange = onAmountChange
+//        )
+    }
+}
