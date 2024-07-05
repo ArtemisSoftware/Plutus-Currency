5b3b1dd6342051200ee94a6cea3a56b693db3045
gustavo <gustavo.isaac@gmail.com>
2024-07-05 17:11:22 +0100

    CurrencyEntity

---
+++ composeApp/src/commonMain/kotlin/data/CurrencyEntity.kt
@@ -0,0 +1,13 @@
+package data
+
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
+import org.mongodb.kbson.ObjectId
+
+
+open  class CurrencyEntity: RealmObject {
+    @PrimaryKey
+    var _id: ObjectId = ObjectId()
+    var code: String = ""
+    var value: Double = 0.0
+}
--- composeApp/src/commonMain/kotlin/data/dto/CurrencyDto.kt
+++ composeApp/src/commonMain/kotlin/data/dto/CurrencyDto.kt
@@ -1,11 +1,12 @@
 package data.dto
+
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class CurrencyDto(
+data  class CurrencyDto (
     @SerialName("code")
     val code: String,
     @SerialName("value")
-    val value: Double
+    var value: Double
 )
